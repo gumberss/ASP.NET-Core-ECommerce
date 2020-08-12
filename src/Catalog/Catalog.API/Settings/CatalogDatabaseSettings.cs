1e5046b4469e4eb4f6d744ef82263f7cba71069e
Gumbers <gustavomarinsuppi@gmail.com>
2020-08-12 19:29:11 -0300

    chore: adding configuration class

---
+++ src/Catalog/Catalog.API/Settings/CatalogDatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Catalog.API.Settings
+{
+    public class CatalogDatabaseSettings : ICatalogDatabaseSettings
+    {
+        public String ConnectionString { get; set; }
+        public String DatabaseName { get; set; }
+        public String CollectionName { get; set; }
+    }
+}
---
+++ src/Catalog/Catalog.API/Settings/ICatalogDatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Catalog.API.Settings
+{
+    public interface ICatalogDatabaseSettings
+    {
+        public String ConnectionString { get; set; }
+        public String DatabaseName { get; set; }
+        public String CollectionName { get; set; }
+    }
+}
