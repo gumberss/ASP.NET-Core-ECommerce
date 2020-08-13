5b33caa391bd7784ed9a77df4af8893868d28b03
Gumbers <gustavomarinsuppi@gmail.com>
2020-08-12 21:58:58 -0300

    chore: adding product repository

---
+++ src/Catalog/Catalog.API/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Catalog.API.Entities;
+
+namespace Catalog.API.Repositories.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProduct(String id);
+        Task<IEnumerable<Product>> GetProductByName(String name);
+        Task<IEnumerable<Product>> GetProductByCategory(String categoryName);
+
+        Task Create(Product product);
+        Task<bool> Update(Product product);
+        Task<bool> Delete(string id);
+    }
+}
