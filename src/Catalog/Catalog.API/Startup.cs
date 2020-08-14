bda5e2e27f32992147bd2a55d3cb280ccc94b42f
Gumbers <gustavomarinsuppi@gmail.com>
2020-08-13 21:30:46 -0300

    chore: adding swagger

--- src/Catalog/Catalog.API/Startup.cs
+++ src/Catalog/Catalog.API/Startup.cs
@@ -16,6 +16,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+
 
 namespace Catalog.API
 {
@@ -40,6 +42,12 @@ namespace Catalog.API
             services.AddTransient<ICatalogContext, CatalogContext>();
 
             services.AddTransient<IProductRepository, ProductRepository>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog API", Version = "v1" });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,7 +57,6 @@ namespace Catalog.API
             {
                 app.UseDeveloperExceptionPage();
             }
-
             app.UseHttpsRedirection();
 
             app.UseRouting();
@@ -60,6 +67,9 @@ namespace Catalog.API
             {
                 endpoints.MapControllers();
             });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog API V1"));
         }
     }
 }
