b47a687b983b5a0107fdb5206e2132efb14c3851
Gumbers <gustavomarinsuppi@gmail.com>
2020-08-15 12:05:43 -0300

    chore: creating entities

---
+++ src/Basket/Basket.API/Entities/BasketCart.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Entities
+{
+    public class BasketCart
+    {
+
+        public String UserName { get; set; }
+
+        public List<BasketCartItem> Items{ get; set; }
+
+        public BasketCart()
+        {
+            Items = new List<BasketCartItem>();
+        }
+
+        public BasketCart(string userName) : this()
+        {
+            UserName = userName;
+        }
+
+        public decimal TOtalPrice => Items.Sum(x => x.Price * x.Quantity);
+    }
+}
---
+++ src/Basket/Basket.API/Entities/BasketCartItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Entities
+{
+    public class BasketCartItem
+    {
+        public int Quantity { get; set; }
+
+        public String Color { get; set; }
+
+        public decimal Price { get; set; }
+
+        public String ProductId { get; set; }
+
+        public String ProductName { get; set; }
+    }
+}
