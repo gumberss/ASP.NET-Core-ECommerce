c6992b08aedd9ffdfafd3da0b98333cfc026d514
Gumbers <gustavomarinsuppi@gmail.com>
2020-08-15 13:29:07 -0300

    chore: adding methods to basket controller

--- src/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Basket/Basket.API/Controllers/BasketController.cs
@@ -1,13 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Net;
 using System.Threading.Tasks;
+using Basket.API.Entities;
+using Basket.API.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Basket.API.Controllers
 {
+    [ApiController]
+    [Route("api/v1/[controller]")]
     public class BasketController : ControllerBase
     {
+        private readonly IBasketRepository _repository;
 
+        public BasketController(IBasketRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("{userName}")]
+        [ProducesResponseType(typeof(BasketCart), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> Get(string userName)
+        {
+            return Ok(await _repository.Get(userName));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(BasketCart), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> Post([FromBody] BasketCart basket)
+        {
+            if(basket is null)
+            {
+                return BadRequest("The Basket to hold should not be null ");
+            }
+
+            return Ok(await _repository.Update(basket));
+        }
+
+        [HttpDelete("{userName}")]
+        [ProducesResponseType(typeof(bool), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> Delete(string userName)
+        {
+            return Ok(await _repository.Delete(userName));
+        }
     }
 }
