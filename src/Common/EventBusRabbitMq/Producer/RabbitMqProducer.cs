ba3bf774ecbdebf4ce2567f95d1a67d96c46aa66
Gumbers <gustavomarinsuppi@gmail.com>
2020-08-16 12:21:32 -0300

    chore: adding producer base class to make the things easier when we need to create a new producer

---
+++ src/Common/EventBusRabbitMq/Producer/RabbitMqProducer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using System.Text.Json;
+using EventBusRabbitMq.Interfaces;
+using RabbitMQ.Client;
+
+namespace EventBusRabbitMq.Producer
+{
+    public abstract class RabbitMqProducer<T> where T : IRabbitEvent
+    {
+        private readonly IRabbitMqConnection _connection;
+
+        protected abstract String Exchange { get; }
+
+        protected RabbitMqProducer(IRabbitMqConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public void Publish(T @event)
+        {
+            using (var channel = _connection.CreteModel())
+            {
+                channel.ExchangeDeclare(Exchange, "fanout", false, false, null);
+
+                var message = JsonSerializer.Serialize(@event);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                IBasicProperties properties = channel.CreateBasicProperties();
+
+                properties.Persistent = false;
+                properties.DeliveryMode = 2;
+
+                channel.ConfirmSelect();
+                channel.BasicPublish(Exchange, "", true, properties, body);
+
+                channel.WaitForConfirmsOrDie();
+
+                channel.BasicAcks += (sender, eventArgs) =>
+                {
+                    Console.WriteLine("Message Sent");
+                };
+
+                channel.ConfirmSelect();
+            }
+        }
+    }
+}
