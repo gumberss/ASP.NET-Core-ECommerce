f5be6bc61849d9febd7781e20b5f836229dad08b
Gumbers <gustavomarinsuppi@gmail.com>
2020-08-16 14:54:35 -0300

    chore: adding automapper and its profile mapping for BasketCheckoutEvent

---
+++ src/Basket/Basket.API/Mapping/BasketCheckoutToBasketCheckoutEventMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Basket.API.Entities;
+using EventBusRabbitMq.Events;
+
+namespace Basket.API.Mapping
+{
+    public class BasketCheckoutToBasketCheckoutEventMapping : Profile
+    {
+        public BasketCheckoutToBasketCheckoutEventMapping()
+        {
+            CreateMap<BasketCheckout, BasketCheckoutEvent>();
+        }
+    }
+}
