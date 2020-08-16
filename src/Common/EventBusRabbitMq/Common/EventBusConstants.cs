85d615ce8222e70966af0730bac72ee873409459
Gumbers <gustavomarinsuppi@gmail.com>
2020-08-16 12:24:00 -0300

    chore: adding event bus constants

---
+++ src/Common/EventBusRabbitMq/Common/EventBusConstants.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace EventBusRabbitMq.Common
+{
+    public static class EventBusConstants
+    {
+        public const String BasketCheckoutExchange = "BasketCheckoutExchange";
+    }
+}
