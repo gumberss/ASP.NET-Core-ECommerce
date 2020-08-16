ec78a141d8b579776b858e9bc676b1dd9d3363a2
Gumbers <gustavomarinsuppi@gmail.com>
2020-08-16 00:08:45 -0300

    chore: creating the common project that contains rabbitmq connections and the events

---
+++ src/Common/EventBusRabbitMq/Interfaces/IRabbitMqConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using RabbitMQ.Client;
+
+namespace EventBusRabbitMq.Interfaces
+{
+    public interface IRabbitMqConnection : IDisposable
+    {
+        bool IsConnected { get; }
+
+        bool TryConnect();
+
+        IModel CreteModel();
+    }
+}
---
+++ src/Common/EventBusRabbitMq/RabbitMqConnection.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using EventBusRabbitMq.Interfaces;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
+
+namespace EventBusRabbitMq
+{
+    public class RabbitMqConnection : IRabbitMqConnection
+    {
+        private readonly IConnectionFactory _connectionFactory;
+        private IConnection _connection;
+        private bool _disposed;
+
+        public RabbitMqConnection(IConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+
+            if (!IsConnected)
+            {
+                TryConnect();
+            }
+        }
+
+        public bool IsConnected => _connection != null && _connection.IsOpen && !_disposed;
+
+        public bool TryConnect()
+        {
+            try
+            {
+                _connection = _connectionFactory.CreateConnection();
+            }
+            catch (BrokerUnreachableException ex)
+            {
+                Thread.Sleep(2000);
+
+                _connection = _connectionFactory.CreateConnection();
+            }
+
+            return IsConnected;
+        }
+
+        public IModel CreteModel()
+        {
+            if (!IsConnected)
+            {
+                throw new InvalidOperationException("No RabbitMq connection - You must connect with RabbitMq first!");
+            }
+
+            return _connection.CreateModel();
+        }
+
+        public void Dispose()
+        {
+            if (_disposed) return;
+
+            _connection.Dispose();
+        }
+    }
+}
