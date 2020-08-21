9bb8134fd4dae68d2083214d273c812d6d84dc96
Gumbers <gustavomarinsuppi@gmail.com>
2020-08-20 21:23:48 -0300

    chore: adding result handlers, because I don't like to throw Exceptions, this is very slow!

---
+++ src/Ordering/Ordering.Application/ResultHandlers/BusinessError.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ordering.Application.ResultHandlers
+{
+    public class BusinessError
+    {
+        public String Message { get; private set; }
+
+        public BusinessError(string message)
+        {
+            Message = message;
+        }
+    }
+}
---
+++ src/Ordering/Ordering.Application/ResultHandlers/Result.cs
@@ -0,0 +1,23 @@
+﻿namespace Ordering.Application.ResultHandlers
+{
+    public class Result<T, TError> where TError : class
+    {
+        public bool Success => Error is null;
+
+        public T Data { get; private set; }
+
+        public TError Error { get; private set; }
+
+        public Result(T data) => Data = data;
+
+        public Result(TError error) => Error = error;
+
+        public static implicit operator T(Result<T, TError> result) => result.Data;
+
+        public static implicit operator TError(Result<T, TError> result) => result.Error;
+
+        public static implicit operator Result<T, TError>(T data) => new Result<T, TError>(data);
+
+        public static implicit operator Result<T, TError>(TError error) => new Result<T, TError>(error);
+    }
+}
