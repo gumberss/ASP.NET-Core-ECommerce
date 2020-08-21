d180e0e6648f1d597c37dbf3be2c8c9251987c96
Gumbers <gustavomarinsuppi@gmail.com>
2020-08-20 21:24:20 -0300

    chore: adding CQRS to ordering microservice

---
+++ src/Ordering/Ordering.Application/Handlers/CheckoutOrderHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Ordering.Application.Commands;
+using Ordering.Application.ExceptionHandlers;
+using Ordering.Application.Mappers;
+using Ordering.Application.Responses;
+using Ordering.Core.Entities;
+using Ordering.Core.Repositories;
+
+namespace Ordering.Application.Handlers
+{
+    public class CheckoutOrderHandler : IRequestHandler<CheckoutOrderCommand, Result<OrderResponse, BusinessError>>
+    {
+        private readonly IOrderRepository _orderRepository;
+
+        public CheckoutOrderHandler(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public async Task<Result<OrderResponse, BusinessError>> Handle(CheckoutOrderCommand request, CancellationToken cancellationToken)
+        {
+            var order = OrderMapper.Mapper.Map<Order>(request);
+
+            if (order is null)
+            {
+                return new BusinessError("The order can't be null");
+            }
+
+            var newOrder = await _orderRepository.AddAsync(order);
+
+            var orderResponse = OrderMapper.Mapper.Map<OrderResponse>(newOrder);
+
+            return orderResponse;
+        }
+    }
+}
---
+++ src/Ordering/Ordering.Application/Mappers/OrderMapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+
+namespace Ordering.Application.Mappers
+{
+    public class OrderMapper
+    {
+        private static readonly Lazy<IMapper> Lazy = new Lazy<IMapper>(() =>
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.ShouldMapProperty = p => p.GetMethod.IsPublic || p.GetMethod.IsAssembly;
+                cfg.AddProfile<OrderMappingProfile>();
+            });
+
+            var mapper = config.CreateMapper();
+
+            return mapper;
+
+        });
+
+        public static IMapper Mapper => Lazy.Value;
+    }
+}
