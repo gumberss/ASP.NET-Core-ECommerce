ad46de4b243bdd09a864a2ff53faeeaf8a7aac2b
Gumbers <gustavomarinsuppi@gmail.com>
2020-09-14 22:31:38 -0300

    chore: configuring program and startup files

--- src/ApiGateway/OcelotAPIGateway/Startup.cs
+++ src/ApiGateway/OcelotAPIGateway/Startup.cs
@@ -16,6 +16,7 @@ namespace OcelotAPIGateway
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddOcelot();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
