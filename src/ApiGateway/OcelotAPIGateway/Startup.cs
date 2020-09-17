07bd0c6970f32b2c527396d235ec0ea5b1a2f078
Gumbers <gustavomarinsuppi@gmail.com>
2020-09-17 20:14:24 -0300

    chore: adding routes to ocelot file

--- src/ApiGateway/OcelotAPIGateway/Startup.cs
+++ src/ApiGateway/OcelotAPIGateway/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
 namespace OcelotAPIGateway
 {
@@ -20,7 +22,7 @@ namespace OcelotAPIGateway
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public async Task Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -36,6 +38,8 @@ namespace OcelotAPIGateway
                     await context.Response.WriteAsync("Hello World!");
                 });
             });
+
+            await app.UseOcelot();
         }
     }
 }
